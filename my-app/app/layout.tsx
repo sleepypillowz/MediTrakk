e645bb668f0a0697b5974d2375cb067834b1f747
Alexander Acojido <alexacojido@gmail.com>
2024-12-11 14:33:29 +0800

    initial commit

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,31 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
+
+
+
+
+
